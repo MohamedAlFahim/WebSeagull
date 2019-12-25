ee56669988f88372bd030e04fcdb346c42281028
MohamedAlFahim <malfahim8@gmail.com>
2019-12-24 21:57:33 -0800

    Use 'let' and 'const' instead of 'var' in several places + handle set inner HTML, add class, remove class, get class list, and get inner HTML Python commands

--- Source/Browser-side/webseagull.js
+++ Source/Browser-side/webseagull.js
@@ -1,4 +1,4 @@
-var websocket = new WebSocket("ws://127.0.0.1:6789/");
+var websocket = new WebSocket('ws://127.0.0.1:6789/');
 
 websocket.onmessage = function (event) {
     data = JSON.parse(event.data);
@@ -8,23 +8,44 @@ websocket.onmessage = function (event) {
             eval(data.code);
             break;
         case 'add_event_listener':
-            var target_element = document.querySelector(data.selector);
+            let target_element = document.querySelector(data.selector);
             // To have the event handler remember the value of data.python_function_name
-            var message_dict_code = `{ 'type': 'event_was_triggered', 'name_of_python_function_to_call': '${data.python_function_name}' }`;
+            const message_dict_code = `{ 'type': 'event_was_triggered', 'name_of_python_function_to_call': '${data.python_function_name}' }`;
             target_element.addEventListener(data.event_type, new Function('event', `
                 websocket.send(JSON.stringify( ${message_dict_code} ));
             `));
             break;
         case 'add_event_listener_to_all':
-            var target_elements = document.querySelectorAll(data.selector);
+            let target_elements = document.querySelectorAll(data.selector);
             target_elements.forEach(element => {
                 // If the element has an ID, the outerHTML can also be sent. Otherwise, send an empty string for the outer_html value.
-                var outer_html_value_code = (element.id === '') ? '""' : `document.getElementById('${element.id}').outerHTML`;
-                var message_dict_code = `{ 'type': 'event_was_triggered', 'name_of_python_function_to_call': '${data.python_function_name}', 'outer_html': ${outer_html_value_code} }`;
+                const outer_html_value_code = (element.id === '') ? '""' : `document.getElementById('${element.id}').outerHTML`;
+                const message_dict_code = `{ 'type': 'event_was_triggered', 'name_of_python_function_to_call': '${data.python_function_name}', 'outer_html': ${outer_html_value_code} }`;
                 element.addEventListener(data.event_type, new Function('event', `
                 websocket.send(JSON.stringify( ${message_dict_code} ));
             `))
             });
+            break;
+        case 'set_inner_html':
+            let target_element = document.querySelector(data.selector);
+            target_element.innerHTML = data.code;
+            break;
+        case 'add_class':
+            let target_element = document.querySelector(data.selector);
+            target_element.classList.add(data.class_name);
+            break;
+        case 'remove_class':
+            let target_element = document.querySelector(data.selector);
+            target_element.classList.remove(data.class_name);
+            break;
+        case 'get_class_list':
+            let target_element = document.querySelector(data.selector);
+            const class_list = target_element.className.split(' ');
+            websocket.send(JSON.stringify({ 'type': 'class_list', 'class_list': class_list }));
+            break;
+        case 'get_inner_html':
+            let target_element = document.querySelector(data.selector);
+            websocket.send(JSON.stringify({ 'type': 'inner_html', 'inner_html': target_element.innerHTML }));
             break;
         default:
             console.error('Unsupported event:', data);
